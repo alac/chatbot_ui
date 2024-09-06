2b2dd0500d5ca54a3d6f90705766118714477138
alac <alac@users.noreply.github.com>
2024-09-05 21:54:55 -0700

    Create ConextDialogPopup with Memory and Author'sNote

--- src/components/ContextPanel.tsx
+++ src/components/ContextPanel.tsx
@@ -2,20 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import {
     DialogContent,
-    DialogFooter,
     DialogHeader,
     DialogOverlay,
     DialogTitle,
     DialogTrigger,
 } from "../ui/dialog"
 import { Button } from "../ui/button"
-import { Separator } from "../ui/separator"
-import { Input } from "../ui/input"
-import { Label } from "../ui/label"
 import { Tab, TabList, TabPanel, Tabs } from "../ui/tabs"
-import Delete from '@spectrum-icons/workflow/Delete';
-import NewItem from '@spectrum-icons/workflow/NewItem';
-import Switch from '@spectrum-icons/workflow/Switch';
+import Edit from '@spectrum-icons/workflow/Edit';
 
 import { storageManager, Conversation } from '../storage';
 
@@ -32,19 +26,19 @@ const ContextPanel = () => {
     },);
 
     useEffect(() => {
-        setHeaderValue(storageManager.currentConversation.memory);
-        setFooterValue(storageManager.currentConversation.authorNote);
+        setMemoryValue(storageManager.currentConversation.memory);
+        setAuthorsNote(storageManager.currentConversation.authorNote);
     }, [contextUpdatedCounter])
 
-    const [headerValue, setHeaderValue] = useState(storageManager.currentConversation.memory);
-    const handleHeaderInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setHeaderValue(event.target.value);
+    const [memoryValue, setMemoryValue] = useState(storageManager.currentConversation.memory);
+    const handleMemoryInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMemoryValue(event.target.value);
         storageManager.currentConversation.memory = event.target.value;
         storageManager.save();
     };
-    const [footerValue, setFooterValue] = useState(storageManager.currentConversation.authorNote);
-    const handleFooterInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setFooterValue(event.target.value);
+    const [authorsNote, setAuthorsNote] = useState(storageManager.currentConversation.authorNote);
+    const handleAuthorsNoteInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setAuthorsNote(event.target.value);
         storageManager.currentConversation.authorNote = event.target.value;
         storageManager.save();
     };
@@ -54,28 +48,31 @@ const ContextPanel = () => {
             <div className="flex items-center">
                 <span className="text-md font-medium">Context</span>
                 <div className="ml-auto">
-                    <span className='corner-button'><NewConversationButton /></span>
+                    <span className='corner-button'>
+                        <ContextDialogButton memory={memoryValue} handleSetMemory={handleMemoryInputChange}
+                            authorsNote={authorsNote} handleSetAuthorsNote={handleAuthorsNoteInputChange} />
+                    </span>
                 </div>
             </div>
             <span className="text-sm">
                 <Tabs>
                     <TabList aria-label="Select Context Field">
-                        <Tab id="Header">Header</Tab>
-                        <Tab id="Footer">Footer</Tab>
+                        <Tab id="Memory">Memory</Tab>
+                        <Tab id="AuthorsNote">Author's Note</Tab>
                     </TabList>
-                    <TabPanel id="Header" className="mt-0">
+                    <TabPanel id="Memory" className="mt-0">
                         <textarea
                             className="sidebar-text-field"
-                            value={headerValue}
-                            onChange={handleHeaderInputChange}
+                            value={memoryValue}
+                            onChange={handleMemoryInputChange}
                             style={{ width: '100%' }}
                         />
                     </TabPanel>
-                    <TabPanel id="Footer" className="mt-0">
+                    <TabPanel id="AuthorsNote" className="mt-0">
                         <textarea
                             className="sidebar-text-field"
-                            value={footerValue}
-                            onChange={handleFooterInputChange}
+                            value={authorsNote}
+                            onChange={handleAuthorsNoteInputChange}
                             style={{ width: '100%' }}
                         />
                     </TabPanel>
@@ -87,44 +84,43 @@ const ContextPanel = () => {
 };
 
 
-const NewConversationButton = () => {
-    const date = new Date();
-    const formattedDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
-    const [conversationName, setConversationName] = useState(formattedDate + " - New Conversation");
+interface ContextDialogProps {
+    memory: string;
+    handleSetMemory: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    authorsNote: string;
+    handleSetAuthorsNote: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setConversationName(event.target.value);
-    };
 
-    const handleCreate = (close: () => void) => {
-        storageManager.newConversation(conversationName);
-        close();
-    }
+const ContextDialogButton = (props: ContextDialogProps) => {
     return (
         <DialogTrigger>
-            <Button size="icon" aria-label='Start a new conversation'><NewItem /></Button>
+            <Button size="icon" aria-label='Start a new conversation'><Edit /></Button>
             <DialogOverlay>
-                <DialogContent className="max-w-[40%] max-h-[90%]" isDismissable={true}>
+                <DialogContent className="max-w-[40%] max-h-[90%] overflow-y-scroll" isDismissable={true}>
                     {({ close }) => (<>
                         <DialogHeader>
-                            <DialogTitle>New Conversation</DialogTitle>
+                            <DialogTitle>Edit Context</DialogTitle>
                         </DialogHeader>
 
-                        <div className="grid grid-cols-5 items-center gap-4">
-                            <Label htmlFor="conversationName" className="text-right">
-                                Title:
-                            </Label>
-                            <Input
-                                id="conversationName"
-                                defaultValue={conversationName}
-                                className="col-span-4"
-                                onChange={handleInputChange}
+                        <div className='grid gap-2'>
+                            <span className="text-md font-medium">Memory:</span>
+                            <textarea
+                                className="sidebar-text-field"
+                                value={props.memory}
+                                onChange={props.handleSetMemory}
+                                style={{ width: '100%' }}
                             />
                         </div>
-
-                        <DialogFooter>
-                            <Button type="submit" onPress={() => { handleCreate(close) }}>Create</Button>
-                        </DialogFooter>
+                        <div className='grid gap-2'>
+                            <span className="text-md font-medium">Author's Note:</span>
+                            <textarea
+                                className="sidebar-text-field"
+                                value={props.authorsNote}
+                                onChange={props.handleSetAuthorsNote}
+                                style={{ width: '100%' }}
+                            />
+                        </div>
                     </>)}
                 </DialogContent>
             </DialogOverlay>
