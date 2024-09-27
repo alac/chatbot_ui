76b2b1d7f865b2d2311f91b9adb8e95a5a7bb8ed
alac <alac@users.noreply.github.com>
2024-09-26 18:13:15 -0700

    MessageList scrolls to the bottom if a new AI message comes in

--- src/components/Chatbox/NaiveConversationChatbox.tsx
+++ src/components/Chatbox/NaiveConversationChatbox.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useImperativeHandle, useState } from 'react';
+import { forwardRef, useImperativeHandle, useState, useRef } from 'react';
 
 import { storageManager, Message } from '../../storage';
 import { ConversationChatboxMethods, ConversationChatboxProps } from './ConversationChatbox';
@@ -7,21 +7,29 @@ import { ChatBubble } from './ChatBubble';
 
 const NaiveConversationChatbox = forwardRef<ConversationChatboxMethods, ConversationChatboxProps>((props: ConversationChatboxProps, ref) => {
     const [_messageListUpdate, setMessageListUpdate] = useState(0)
+    const messageListRef = useRef<HTMLDivElement>(null);
 
     useImperativeHandle(ref, () => ({
         appendMessage: (_message: Message) => {
             setMessageListUpdate((v) => v + 1)
+            if (messageListRef.current) {
+                messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+            }
         },
         deleteMessage: (_messageKey: string) => {
             setMessageListUpdate((v) => v + 1)
         },
         updateMessages: (_callback: (message: Message) => Message) => {
             setMessageListUpdate((v) => v + 1)
+            if (messageListRef.current) {
+                messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+            }
         },
     }));
 
     return <>
         <div
+            ref={messageListRef}
             style={{ maxHeight: "100%", minHeight: "100%", overflowY: "scroll" }}
         >
             {storageManager.currentConversation.messages.map((m: Message, _index: number) => {
