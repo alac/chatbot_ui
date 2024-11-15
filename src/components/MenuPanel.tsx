b09bd145082e1a2f982b426dfda26b44ed46965e
alac <alac@users.noreply.github.com>
2024-11-14 17:54:47 -0800

    Update MenuPanel.tsx

--- src/components/MenuPanel.tsx
+++ src/components/MenuPanel.tsx
@@ -25,25 +25,6 @@ import {
     MenuTrigger,
 } from "../ui/menu"
 
-export function MenuDemo() {
-    return (
-        <MenuTrigger>
-            <Button aria-label="Menu" size="icon" variant="outline">
-                ☰
-            </Button>
-            <MenuPopover>
-                <Menu>
-                    <MenuItem onAction={() => alert("open")}>Open</MenuItem>
-                    <MenuItem onAction={() => alert("rename")}>Rename…</MenuItem>
-                    <MenuItem onAction={() => alert("duplicate")}>Duplicate</MenuItem>
-                    <MenuItem onAction={() => alert("share")}>Share…</MenuItem>
-                    <MenuItem onAction={() => alert("delete")}>Delete…</MenuItem>
-                </Menu>
-            </MenuPopover>
-        </MenuTrigger>
-    )
-}
-
 
 
 const MenuPanel = () => {
