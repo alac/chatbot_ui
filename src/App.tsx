6501bbfa519d4492a79760f96c799751b0b68338
alac <alac@users.noreply.github.com>
2024-11-17 20:44:24 -0800

    Enable Tools panels

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
                   id="ToolsBar"
                   order={1}
                 >
-                  {/* <div className="panel m-1 px-2 py-2 rounded-md bg-primary text-primary-foreground">
+                  <div className="panel m-1 px-2 py-2 rounded-md bg-primary text-primary-foreground">
                     <div className="flex items-center">
                       <span className="text-md font-medium">Summarize</span>
                       <div className="ml-auto corner-button"></div>
@@ -74,6 +74,13 @@ function App() {
                   <div className="panel m-1 px-2 py-2 rounded-md bg-primary text-primary-foreground">
                     <div className="flex items-center">
                       <span className="text-md font-medium">Objectives</span>
+                      <div className="ml-auto corner-button"></div>
+                    </div>
+                  </div>
+
+                  <div className="panel m-1 px-2 py-2 rounded-md bg-primary text-primary-foreground">
+                    <div className="flex items-center">
+                      <span className="text-md font-medium">Choices</span>
                       <div className="ml-auto corner-button"></div>
                     </div>
                   </div>
@@ -92,7 +99,7 @@ function App() {
                       </span>
                       <div className="ml-auto corner-button"></div>
                     </div>
-                  </div> */}
+                  </div>
                 </Panel>
                 <PanelResizeHandle />
               </>
